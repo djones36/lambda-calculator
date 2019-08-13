fdd840a7c59900fbc58db3c3f85481189313f85b
Daniel Jones <daniel.jones2000@gmail.com>
2019-08-13 15:37:20 -0700

    marked up div classes

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.jsx
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const OperatorButton = () => {
   return (
-    <>
+    <button className="operator-button">
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+    </button>
   );
 };
